8d026fcd82c412c50ccdc938dfd7a2d3fefe76cf
yuukiiwai <yuuki.20020217@gmail.com>
2023-10-04 10:09:47 +0900

    add 基本的な.envの追加

--- dlbucket/settings.py
+++ dlbucket/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k2ue0m6!e8yze#rjt((i*6r+m%#$^xeyz*l(*by09fg$_vgw$m'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") != "False"
 
 ALLOWED_HOSTS = []
 
